ca62ff848b82062a535a26b7f1101015b5806fda
dipeshkumardahal <dipeshkumardahal@outlook.com>
2021-05-20 14:42:19 +0545

    Lists in dart

---
+++ list.dart
@@ -0,0 +1,14 @@
+void main() {
+  //Making some List
+  List<String> sampleList = ['List item 1', 'Llist Item 2', 'List Item 3'];
+  var sampleListTwo = ['var list one', 'var list two', 'var list three'];
+
+  //printing some list items
+  print(sampleList[0] +
+      "Is first item in first list " +
+      sampleListTwo.last +
+      " is lat of second list");
+//Printing Whole lists
+  print(sampleList);
+  print(sampleListTwo);
+}
