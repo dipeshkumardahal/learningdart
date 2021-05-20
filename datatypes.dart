65a59e8855b9e17118e8760e11b42b4b0c7846b2
dipeshkumardahal <dipeshkumardahal@outlook.com>
2021-05-20 15:10:20 +0545

    constant datatypes

--- datatypes.dart
+++ datatypes.dart
@@ -10,7 +10,10 @@ void main() {
   var dadAge = 58;
   var sisters = ['Dipa', 'Dikshya'];
   var dob = DateTime(1995);
+  //the constant is non changeable variable
+  const gotra = "Batsa";
+  //gotra = "fdf";  Constant canot be changed latar
   //Printing Some Variables
   print(
-      "Hello World I am ${name}.\n born in ${dob.year} My age is ${age}.\n I got ${slcScore} in SLC.\n I like ${hobby}.\n I am single that is ${isSingle}.\n I have ${sisters.length} Sisters they are ${sisters}");
+      "Hello World I am ${name}.\n born in ${dob.year} My age is ${age}.\n I got ${slcScore} in SLC.\n I like ${hobby}.\n I am single that is ${isSingle}.\n I have ${sisters.length} Sisters they are ${sisters} and our gotra is ${gotra}");
 }
