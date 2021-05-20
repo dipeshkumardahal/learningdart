a8ed4e16998eb489c531712976544a6ba81e20cb
dipeshkumardahal <dipeshkumardahal@outlook.com>
2021-05-20 15:15:13 +0545

    math calculations

---
+++ math.dart
@@ -0,0 +1,15 @@
+void main() {
+  //declaring two numbers
+  var a = 9;
+  var b = 20;
+
+  //performing calculation
+  var sum = a + b;
+  var minus = b - a;
+  var mul = b * a;
+  var div = b / a;
+  var modu = b % a;
+
+  print(
+      "The first number is ${a} \nThe Second number is ${b} \n the sum is ${sum} minus is ${minus}\n multification is ${mul} division is ${div}\n and the modulas of ${b} and ${a} is ${modu}");
+}
