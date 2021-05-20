37745d33461d7a9eeb66cb387d929ddebb27f7f2
dipeshkumardahal <dipeshkumardahal@outlook.com>
2021-05-20 17:09:55 +0545

    switch statements

---
+++ switch.dart
@@ -0,0 +1,13 @@
+void main() {
+  var way = "nath";
+  switch (way) {
+    case "nath":
+      print("Aum Adesh");
+      break;
+    case "naga":
+      print("Aum namo narayana");
+      break;
+    default:
+      print("Jay Bhole");
+  }
+}
