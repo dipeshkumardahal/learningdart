5ee78b875916b9097ba7cf2fabdc55586d539be1
dipeshkumardahal <dipeshkumardahal@outlook.com>
2021-05-20 14:04:33 +0545

    Datatypes

---
+++ datatypes.dart
@@ -0,0 +1,16 @@
+void main() {
+  //Making Some Variables
+  String name = "Dipesh Dahal";
+  int age = 25;
+  double slcScore = 61.25;
+  List<String> hobby = ['Computing', 'Music', 'Tourism'];
+  bool isSingle = true;
+  //var works for all data types
+  var fathersName = "Suresh Raj Dahal";
+  var dadAge = 58;
+  var sisters = ['Dipa', 'Dikshya'];
+
+  //Printing Some Variables
+  print(
+      "Hello World I am ${name}.\n My age is ${age}.\n I got ${slcScore} in SLC.\n I like ${hobby}.\n I am single that is ${isSingle}.\n I have ${sisters.length} Sisters they are ${sisters}");
+}
