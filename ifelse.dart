cd42eb37d4b285baa2740321289d64b93cc73173
dipeshkumardahal <dipeshkumardahal@outlook.com>
2021-05-20 16:58:45 +0545

    if else

---
+++ ifelse.dart
@@ -0,0 +1,10 @@
+void main() {
+  int age = 30;
+  if (age < 18) {
+    print("opps under age");
+  } else if (age <= 40) {
+    print("Welcome To the women hunting club");
+  } else {
+    print("Time to retire Uncle");
+  }
+}
